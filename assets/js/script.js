5582bcb3de56f412b7e85f2683810601683037b3
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-05-28 19:40:48 -0500

    added start quiz function and improved css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,86 +3,55 @@ var beginEl = document.querySelector("#btn-begin");
 var textEl = document.querySelector("#questionContainer");
 var answerEl = document.querySelector("#correctWrong");
 var timerEl = document.querySelector("#timeSet")
+var questionEl = document.querySelector("#questions-content")
+const questions = [
+    { 
+        question: "Where in the HTML does the JavaScript code go?",
+        answers: [
+            {text: "head", correct: false}, {text: "div", correct: false}, {text: "section", correct:false },{text:"body", correct: true} 
+        ]
+    
+    },
+    { 
+        question: "What are Arrays in JavaScript defined by?",
+        answers: [
+            {text: "ordered list of values", correct:true}, {text:"ordered list of variables",correct:false}, {text: "ordered list of strings", correct:false}, {text:"ordered list of functions", correct: false}
+        ]
 
-var quizQuestions = [
-    {
-        Q: "Where in the HTML does the JavaScript code go?",
-        options: ["head", "div", "section", "body"],
-        answer: "body"
     },
-    {
-        Q: "What are Arrays in JavaScript defined by?",
-        options: ["ordered list of values", "ordered list of variables", "ordered list of strings", "ordered list of functions"],
-        answer: "ordered list of values"
+    { 
+        question: "When ending code in Javascript what is used to close it out? ",
+        answers: [
+            {text:"comma",correct:false}, {text:"curly bracket", correct: false }, {text:"semi-colon",correct:true},{text:"colon",correct:false} 
+        ]
     },
-    {
-        Q: "When ending code in Javascript what is used to close it out? ",
-        options: ["comma", "curly bracket", "semi-colon", "colon"],
-        answer: "semi-colon"
+    { 
+        question: "What are function and var known as in Javascript?",
+        answers: [
+            {text:"variables", correct:false},{text:"data", correct:false}, {text:"elements",correct:false},{text:"declaration statements", correct:true}
+        ]
     },
-    {
-        Q: "What are function and var known as in Javascript?",
-        options: ["variables", "data", "elements", "declaration statements"],
-        answer: "declaration statements"
-    },
-    {
-        Q: "In this example of an array var pokemon = ['Eevee','Vaporeon', 'Jolteon', 'Flareon', 'Espeon','Umbreon'] what is the element of[4] ",
-        options: ["Flareon", "Jolteon", "Espeon", "Umbreon"],
-        answer: "Espeon"
-
+    { 
+        question: "In this example of an array var pokemon = ['Eevee','Vaporeon', 'Jolteon', 'Flareon', 'Espeon','Umbreon'] what is the element of[4] ",
+        answers: [
+            {text:"Flareon", correct:false}, {text:"Jolteon", correct: false}, {text: "Espeon", correct:true}, {text:"Umbreon", correct:false}
+        ]
     }
 ];
 
-
-// highscore 
-var score = 0;
-// start quiz 
 var startQuiz = function () {
     beginEl.remove();
-    renderQuestions();
-    timerCountdown();
+    textEl.remove();
+    questionEl.classList.remove("hidden")
+    nextQuestion();
 };
-var counter = 0;
-var renderQuestions = function () {
-    textEl.textContent = quizQuestions[counter].Q
-    renderChoices(counter);
-    counter++;
-    
+var nextQuestion = function() {
 
-};
-var renderChoices = function (questionCounter) {
-    for (var i = 0; i < quizQuestions[questionCounter].options.length; i++) {
-        // make a button for each
-        var optionBtn = document.createElement("button");
-        optionBtn.setAttribute("id","btn_id")
-        optionBtn.innerText = quizQuestions[questionCounter].options[i];
-        optionBtn.addEventListener("click", checkAnswer(questionCounter));
-        optionBtn.classList.add("btn_class")
-        document.body.appendChild(optionBtn);
-        textEl.appendChild(optionBtn);
-        console.log('buttons got loaded with eventlistener')
-        // btn.addEventListener("click",)
-
-        // tried puting this into it's own function called checkAnswer on line 59 i had it called (click, checkAnswer) but because these buttons were created in this for loop i can't reference them back in that function
-        // so now i'm trying to make it go continue just from this function but still not getting a correct console log or textContent of correct
-    }
-   
-  
-};
-var checkAnswer = function (choicesCounter) {
-    if (quizQuestions[choicesCounter].options === quizQuestions[choicesCounter].answer){
-        console.log('correct');
-        return answerEl.textContent= "correct!";
-
-    }
-    else (quizQuestions[choicesCounter].options !== quizQuestions[choicesCounter].answer) 
-        answerEl.textContent = "wrong";
-    
+}
+var showQuestions = function(question){
+    questionEl.innerText = question.question
 }
 
-
-
-// };
 // timer settings
 var quizTime = 10;
 var penaltyTime = 10;
@@ -90,7 +59,7 @@ var penaltyTime = 10;
 var timerCountdown = function () {
     timerEl.innerHTML= quizTime;
     var countTimer = setInterval(function () {
-        timerEl.innerHTML=quizTime --;
+        timerEl.innerHTML = quizTime; timerEl.innerHTML=quizTime --;
         
         if (quizTime <= 0) {
             clearInterval(countTimer);
